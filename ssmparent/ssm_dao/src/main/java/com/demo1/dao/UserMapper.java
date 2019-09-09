82cd02c0ba274121c38a6e3f22a9409bd1ad31d5
yebo <932104018@qq.com>
2019-09-09 14:35:32 +0800

    新加了一个Mapper

---
+++ ssmparent/ssm_dao/src/main/java/com/demo1/dao/UserMapper.java
@@ -0,0 +1,8 @@
+package com.demo1.dao;
+
+import com.demo1.pojo.User;
+
+public interface UserMapper {
+
+    void adduser(User user);
+}
--- ssmparent/ssm_dao/src/main/java/com/demo1/pojo/User.java
+++ ssmparent/ssm_dao/src/main/java/com/demo1/pojo/User.java
@@ -5,6 +5,33 @@ public class User {
     private  int id;
     private  String name;
     private  String password;
+    private Integer sex;
+    private  Integer age;
+    private  String address;
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public int getId() {
         return id;
